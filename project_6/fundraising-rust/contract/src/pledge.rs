7ca02934b8d9407771fa111eb9e1c428df88237c
mariia.synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2023-05-13 23:26:44 +0200

    added payable function

--- project_6/fundraising-rust/contract/src/pledge.rs
+++ project_6/fundraising-rust/contract/src/pledge.rs
@@ -1,10 +1,12 @@
 use crate::Contract; // crate like a package import
 use crate::ContractExt; // ContractExt -> instead lib.rs file (aka contract)
 
-use near_sdk::{ AccountId, near_bindgen };
+use near_sdk::{ AccountId, near_bindgen, env, Balance, Promise };
 use near_sdk::json_types::U128;
 use near_sdk::serde::Serialize;
 use near_sdk::borsh::{ self, BorshDeserialize, BorshSerialize };
+
+pub const STORAGE_COST: u128 = 1_000_000_000_000_000_000_000;
 
 #[near_bindgen] // treat like a smart contract
 #[derive(BorshDeserialize, BorshSerialize, Serialize)]
@@ -23,7 +25,31 @@ impl Contract {
     pub fn get_pledge_for_account(&self, account_id: AccountId) -> Pledge {
         Pledge {
             account_id: account_id.clone(),
-            total_amount: U128(self.pledges.get(&account_id).unwrap_or(0))
+            total_amount: U128(self.pledges.get(&account_id).unwrap_or(0)),
         }
+    }
+
+    #[payable]
+    pub fn pledge(&mut self) -> U128 {
+        let pledger: AccountId = env::predecessor_account_id();
+        let pledge_amount: Balance = env::attached_deposit();
+
+        let mut pledge_so_far = self.pledges.get(&pledger).unwrap_or(0);
+
+        let to_transfer: Balance = if pledge_so_far == 0 {
+            assert!(pledge_amount > STORAGE_COST, "Attached at least {} yoctoNEAR", STORAGE_COST);
+
+            pledge_amount - STORAGE_COST
+        } else {
+            pledge_amount
+        };
+
+        pledge_so_far += pledge_amount;
+
+        self.pledges.insert(&pledger, &pledge_so_far);
+
+        Promise::new(self.fundraiser.clone()).transfer(to_transfer);
+
+        U128(pledge_so_far)
     }
 }
