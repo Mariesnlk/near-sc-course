5f1283fa5b18a6e75421963f15e767ddae6dc188
mariia.synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2023-05-15 20:14:03 +0200

    added get

--- project_21/post-rust/contract/src/lib.rs
+++ project_21/post-rust/contract/src/lib.rs
@@ -86,4 +86,55 @@ impl SocialNetworking {
     pub fn get_all_posts(&self) -> Vec<(u128, Post)> {
         self.posts.to_vec()
     }
+
+    pub fn add_post_to_my_liked(&mut self, sender_id: AccountId, post: Post) {
+        let mut likes: Vec<Post>;
+        if let None = self.likes_by_user_id.get(&sender_id) {
+            likes = Vec::<Post>::new();
+        } else {
+            likes = self.likes_by_user_id.get(&sender_id).unwrap();
+        }
+        likes.push(post);
+        self.likes_by_user_id.insert(&sender_id, &likes);
+    }
+
+    pub fn like_a_post(&mut self, post_id: u128) -> Post {
+        if let None = self.posts.get(&post_id) {
+            return Post {
+                id: post_id,
+                title: "No post found at that Id".to_string(),
+                description: "No post found at that Id".to_string(),
+                tags: Vec::<String>::new(),
+                media: "No post found at that Id".to_string(),
+                users_who_liked: Vec::<AccountId>::new(),
+                owner_id: env::signer_account_id(),
+            };
+        }
+
+        let mut post = self.posts.get(&post_id).unwrap();
+        let sender_id = env::signer_account_id();
+        post.users_who_liked.push(sender_id.clone());
+        self.posts.insert(&post_id, &post);
+
+        self.add_post_to_my_liked(sender_id, post.clone());
+
+        return post;
+    }
+
+    pub fn get_my_liked_post(&self) -> Vec<Post> {
+        let sender_id = env::signer_account_id();
+        if let None = self.likes_by_user_id.get(&sender_id) {
+            return Vec::<Post>::new();
+        }
+        let my_liked_posts = self.likes_by_user_id.get(&sender_id);
+        return my_liked_posts.unwrap();
+    }
+
+    pub fn get_posts_by_tag(&self, tag: String) -> Vec<Post> {
+        if let None = self.posts_by_tag.get(&tag) {
+            return Vec::<Post>::new();
+        }
+
+        return self.posts_by_tag.get(&tag).unwrap();
+    }
 }
